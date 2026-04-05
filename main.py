baff7aa3b595094775f011d3c8c29650b90666c1
Adrian Prach <263222609+adrianprach@users.noreply.github.com>
2026-04-05 15:53:38 +0930

    feat: add run py file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import argparse
@@ -15,14 +15,20 @@ parser = argparse.ArgumentParser(description="EOS prompt")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 
+
 def main():
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    attempt = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    attempt = client.models.generate_content(
+        model="gemini-2.5-flash", contents=messages
+    )
     if attempt.usage_metadata is None:
         raise RuntimeError("Fail to fetch usage metadata.")
     # attempt.usage_metadata.thoughts_token_count
-    prompt_token, response_token = attempt.usage_metadata.prompt_token_count, attempt.usage_metadata.candidates_token_count
+    prompt_token, response_token = (
+        attempt.usage_metadata.prompt_token_count,
+        attempt.usage_metadata.candidates_token_count,
+    )
     # attempt.usage_metadata.prompt_token_count
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
